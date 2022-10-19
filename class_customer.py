6bce93535ced38ccf0d23bb5edc09cace84d3f61
DavidLiebeFreunde <davidahamed@posteo.de>
2022-10-19 11:56:12 +0200

    adding customer class

---
+++ class_customer.py
@@ -0,0 +1,11 @@
+class Customer:
+    """
+    a single customer that moves through the supermarket
+    in a MCMC simulation
+    """
+    
+    def __init__(self):        
+        return
+    
+    def send_shoppping(self):
+        return
